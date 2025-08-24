db0f213456e79728ad29a9f4b7c2e553623a24d7
nikhil misal <nik0misal@gmail.com>
2025-08-24 16:33:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 3000);
  
 module.exports = app;
+
+
+
+
+#or
+
+
+ 
+var express = require('express');
+ 
+var app = express();//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('changed not ');
+});
+
+app.get('/getreels', function (req, res) {
+ res.send('{"id":1,"Name":"nikhil","caption":"is anyone here"} ');
+});
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
+ 
